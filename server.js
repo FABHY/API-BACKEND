ae8776e52a05c64ec13478a28ff33ff7da87be5b
FABHY <fabiohypolito2021@outlook.com>
2024-08-08 14:52:10 -0300

    Segundo commit

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express()
 app.use(express.json ())
 
 
-const users = []
-
 app.post('/usuarios',async (req,res) => {
 
    await prisma.user.create({
@@ -24,9 +22,58 @@ app.post('/usuarios',async (req,res) => {
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req,res) =>{
+app.get('/usuarios', async (req,res) =>{
+
+    let user= []
+
+    if(req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+
+    }else{
+        users = await prisma.user.findMany
+
+    }
+
+    const users = await prisma.user.findMany()
+
+
     res.status(200).json(users)
 })
+
+app.put('/usuarios/:id',async (req,res) => {
+
+    
+    await prisma.user.update({
+     where:{
+       id:req.params.id
+
+      },
+     data: {
+        email:req.body.email,
+        name:req.body.name,
+        age:req.body.age
+ 
+     }
+ 
+    })
+ 
+     res.status(201).json(req.body)
+ })
+
+ app.delete('/usuarios/:id', async(req,res) => {
+    await prisma.user.delete({
+        where:{
+            id:req.params.id
+        }
+    })
+    res.status(200).json({message: "Usuario deletado com sucesso"})
+ })
 
 app.listen(3000)
 
